3555d5a4d807e8ecb1c38e359f3cfd6f8a05decf
Peter8977 <Peter.loevebo@gmail.com>
2026-05-18 22:02:04 +0200

    Medicin Checkbox API opkobling og miniturisering af Status logik.

--- Slottet/API/Controllers/MedicinController.cs
+++ Slottet/API/Controllers/MedicinController.cs
@@ -1,5 +1,6 @@
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Domain.Entities;
 
 namespace API.Controllers;
 
@@ -42,4 +43,36 @@ public class MedicinController : ControllerBase
 
         
     }
+
+    // PUT /api/medicin/{id}
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateMedicin(int id, [FromBody] UpdateMedicinRequest request)
+    {
+        var medicin = await _db.Medicins.FindAsync(id);
+
+        if (medicin is null)
+            return NotFound();
+
+        medicin.Time = request.Time;
+        await _db.SaveChangesAsync();
+
+        //Log medicin time updated
+        await _db.AuditLogs.AddAsync(new AuditLog
+        {
+            LogType  = "Activity",
+            Action   = "Medicin tid opdateret",
+            Entity   = "Medicin",
+            EntityId = id.ToString(),
+            UserId   = null,
+            UserName = "unknown"
+        });
+        await _db.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
+
+public class UpdateMedicinRequest
+{
+    public DateTime Time { get; set; }
 }
